bb921f3cc9a3ba6b9a5f58d5eb0fe44efa3e0c62
Romário Getúlio Vargas <romario.geomais@gmail.com>
2020-11-28 16:42:45 -0300

    feat: recipe return error if no Ingredients

--- src/presentation/controllers/recipe.ts
+++ src/presentation/controllers/recipe.ts
@@ -1,7 +1,8 @@
 export class RecipeController {
   handle (httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      params: new Error('Ausência de ingredientes como parâmtetros: i')
     }
   }
 }
