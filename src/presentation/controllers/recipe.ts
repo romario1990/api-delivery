070dd8c2f3ca652d85ac796e6fdff23095134917
Romário Getúlio Vargas <romario.geomais@gmail.com>
2020-11-28 17:25:50 -0300

    refactor: create interface controller

--- src/presentation/controllers/recipe.ts
+++ src/presentation/controllers/recipe.ts
@@ -1,8 +1,9 @@
 import { HttpResponse, HttpRequest } from './../protocols/http'
 import { MissingParamError } from './../errors/missing-param-error'
 import { badRequest } from './../helpers/http-helper'
+import { Controller } from '../protocols/controller'
 
-export class RecipeController {
+export class RecipeController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.params.i || httpRequest.params.i === '') {
       return badRequest(new MissingParamError('i = ingredientes'))
