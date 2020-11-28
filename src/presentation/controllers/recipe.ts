f6bf7eaed75591a77890c7ca4f1e0744a40a7f79
Romário Getúlio Vargas <romario.geomais@gmail.com>
2020-11-28 17:21:36 -0300

    refactor: create badRequest

--- src/presentation/controllers/recipe.ts
+++ src/presentation/controllers/recipe.ts
@@ -1,13 +1,11 @@
 import { HttpResponse, HttpRequest } from './../protocols/http'
 import { MissingParamError } from './../errors/missing-param-error'
+import { badRequest } from './../helpers/http-helper'
 
 export class RecipeController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.params.i || httpRequest.params.i === '') {
-      return {
-        statusCode: 400,
-        body: new MissingParamError('i = ingredientes')
-      }
+      return badRequest(new MissingParamError('i = ingredientes'))
     }
   }
 }
