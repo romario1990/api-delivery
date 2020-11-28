cc59d3346368c77bbd8420b8af2be01b040ec7cc
Romário Getúlio Vargas <romario.geomais@gmail.com>
2020-11-28 16:43:16 -0300

    test: recipe return error if no Ingredients

--- src/presentation/controllers/recipe.spec.ts
+++ src/presentation/controllers/recipe.spec.ts
@@ -1,7 +1,7 @@
 import { RecipeController } from './recipe'
 
 describe('Recipe Controller', () => {
-  test('Should return 400 if no ingredients is provided', () => {
+  test('Deve retornar 400 se nenhum ingrediente for fornecido', () => {
     const recipeController = new RecipeController()
     const httpRequest = {
       params: {
@@ -10,5 +10,6 @@ describe('Recipe Controller', () => {
     }
     const httpResponse = recipeController.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.params).toEqual(new Error('Ausência de ingredientes como parâmtetros: i'))
   })
 })
