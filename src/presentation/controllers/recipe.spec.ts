36880f9102d5b4bb3de030463e80a34e6b8c6cba
Romário Getúlio Vargas <romario.geomais@gmail.com>
2020-11-29 09:48:21 -0300

    refactor: extraido criação RecipeController

--- src/presentation/controllers/recipe.spec.ts
+++ src/presentation/controllers/recipe.spec.ts
@@ -1,9 +1,13 @@
 import { RecipeController } from './recipe'
 import { MissingParamError } from './../errors/missing-param-error'
 
+const MakeRecipeController = (): RecipeController => {
+  return new RecipeController()
+}
+
 describe('Recipe Controller', () => {
   test('Deve retornar 400 se nenhum ingrediente for fornecido', () => {
-    const recipeController = new RecipeController()
+    const recipeController = MakeRecipeController()
     const httpRequest = {
       params: {
         i: ''
